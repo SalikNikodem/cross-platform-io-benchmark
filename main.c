ea17f798dbe790dd75da2a0678ff4ed1d6dbddf7
SalikNikodem <saliknikodem@gmail.com>
2026-05-20 16:42:21 +0200

    initial funciotns

--- main.c
+++ main.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(__linux__)
-
-#elif defined(_WIN32) || defined(__APPLE__)
-#endif
+// #if defined(__linux__)
+// #elif defined(_WIN32) || defined(__APPLE__)
+// #endif
 
 #define DEFAULT_OUTPUT "data.log"
 #define BUFFER_SIZE_LINE 2048
 #define BUFFER_SIZE_BLOCK 4096
 
+
 int process_by_char(FILE *in_file, FILE *out_screen, FILE *out_file) {
     return EXIT_SUCCESS;
 }
+
 
 int process_by_line(FILE *in_file, FILE *out_screen, FILE *out_file) {
     return EXIT_SUCCESS;
@@ -28,14 +29,11 @@ int process_by_block(FILE *in_file, FILE *out_screen, FILE *out_file) {
 void print_output_file_content(const char *filename, FILE *out_file) {
 }
 
-
 int main(int argc, char *argv[]) {
 
-    #if defined(__linux__)
-
-    #else
-
-    #endif
+    // #if defined(__linux__)
+    // #else
+    // #endif
 
     return EXIT_SUCCESS;
 }

be3eb1583898854f259183e91eed6f6beb695da0
SalikNikodem <saliknikodem@gmail.com>
2026-05-20 16:25:07 +0200

    initial functions

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(__linux__)
+
+#elif defined(_WIN32) || defined(__APPLE__)
+#endif
+
+#define DEFAULT_OUTPUT "data.log"
+#define BUFFER_SIZE_LINE 2048
+#define BUFFER_SIZE_BLOCK 4096
+
+int process_by_char(FILE *in_file, FILE *out_screen, FILE *out_file) {
+    return EXIT_SUCCESS;
+}
+
+int process_by_line(FILE *in_file, FILE *out_screen, FILE *out_file) {
+    return EXIT_SUCCESS;
+}
+
+
+int process_by_block(FILE *in_file, FILE *out_screen, FILE *out_file) {
+    return EXIT_SUCCESS;
+}
+
+
+void print_output_file_content(const char *filename, FILE *out_file) {
+}
+
+
+int main(int argc, char *argv[]) {
+
+    #if defined(__linux__)
+
+    #else
+
+    #endif
+
+    return EXIT_SUCCESS;
+}

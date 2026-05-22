cc1ecefc6fca2df03c5769d7e888f17cd35fa1d2
SalikNikodem <saliknikodem@gmail.com>
2026-05-22 16:13:06 +0200

    argument checking and system check

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if defined(__linux__)
+    #include <limits.h>
+    #define IS_LINUX 1
+#else
+    #define IS_LINUX 0
+#endif
+
 #define DEFAULT_OUTPUT "data.log"
 #define BUFFER_SIZE_LINE 2048
 #define BUFFER_SIZE_BLOCK 4096
@@ -26,5 +33,13 @@ void print_output_file_content(const char *filename, FILE *out_file) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc < 4 || argc > 5) {
+            fprintf(stderr, "Błąd: Niepoprawna liczba argumentów!\n");
+            return EXIT_FAILURE;
+        }
+
+
+
+
     return EXIT_SUCCESS;
 }

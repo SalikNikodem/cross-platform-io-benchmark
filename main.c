9b6ed2899f726bddc2d522b886a1d7f6adb174c5
SalikNikodem <saliknikodem@gmail.com>
2026-05-25 10:57:05 +0200

    main commit

--- main.c
+++ main.c
@@ -2,17 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(__linux__)
-    #include <limits.h>
-    #define IS_LINUX 1
-#else
-    #define IS_LINUX 0
-#endif
-
 #define DEFAULT_OUTPUT "data.log"
 #define BUFFER_SIZE_LINE 2048
 #define BUFFER_SIZE_BLOCK 4096
-
 
 int process_by_char(FILE *in_file, FILE *out_screen, FILE *out_file);
 int process_by_line(FILE *in_file, FILE *out_screen, FILE *out_file);
@@ -21,11 +13,11 @@ void print_output_file_content(const char *filename, FILE *out_file);
 
 int main(int argc, char *argv[]) {
     if (argc < 4 || argc > 5) {
-            fprintf(stderr, "Błąd: Niepoprawna liczba argumentów!\n");
-            return EXIT_FAILURE;
-        }
+        fprintf(stderr, "Błąd: Niepoprawna liczba argumentów!\n");
+        fprintf(stderr, "Użycie: %s <mode> <in1> <in2> [out]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-        
     char *mode = argv[1];
     char *in1_name = argv[2];
     char *in2_name = argv[3];
@@ -35,81 +27,109 @@ int main(int argc, char *argv[]) {
     FILE *in2 = NULL;
     FILE *out = NULL;
 
+    if (strcmp(mode, "3") == 0) {
+        in1 = fopen(in1_name, "rb");
+        in2 = fopen(in2_name, "rb");
+        out = fopen(out_name, "a+b"); 
+    } else {
+        in1 = fopen(in1_name, "r");
+        in2 = fopen(in2_name, "r");
+        out = fopen(out_name, "a+");  
+    }
 
+    if (!in1 || !in2 || !out) {
+        fprintf(stderr, "Błąd otwarcia strumieni!\n");
+        perror("fopen");
+        
+        if (in1) fclose(in1);
+        if (in2) fclose(in2);
+        if (out) fclose(out);
+        return EXIT_FAILURE;
+    }
+
+    int status = EXIT_SUCCESS;
+
+    printf("%s: ", in1_name);
+    if (strcmp(mode, "1") == 0) {
+        status = process_by_char(in1, stdout, out);
+    } else if (strcmp(mode, "2") == 0) {
+        status = process_by_line(in1, stdout, out);
+    } else if (strcmp(mode, "3") == 0) {
+        status = process_by_block(in1, stdout, out);
+    } else {
+        fprintf(stderr, "Nieznany tryb: %s\n", mode);
+        fclose(in1); fclose(in2); fclose(out);
+        return EXIT_FAILURE;
+    }
+    printf("\n");
+
+    if (status != EXIT_SUCCESS) {
+        fclose(in1); fclose(in2); fclose(out);
+        return EXIT_FAILURE;
+    }
+
+    printf("%s: ", in2_name);
+    if (strcmp(mode, "1") == 0) {
+        status = process_by_char(in2, stdout, out);
+    } else if (strcmp(mode, "2") == 0) {
+        status = process_by_line(in2, stdout, out);
+    } else if (strcmp(mode, "3") == 0) {
+        status = process_by_block(in2, stdout, out);
+    }
+    printf("\n");
+
+    if (status != EXIT_SUCCESS) {
+        fclose(in1); fclose(in2); fclose(out);
+        return EXIT_FAILURE;
+    }
+
+    print_output_file_content(out_name, out);
+
+    fclose(in1);
+    fclose(in2);
+    fclose(out);
 
     return EXIT_SUCCESS;
 }
 
 int process_by_char(FILE *in_file, FILE *out_screen, FILE *out_file) {
     int c;
-
     while ((c = fgetc(in_file)) != EOF) {
         fputc(c, out_screen);
         fputc(c, out_file);
     }
-
-    if (ferror(in_file)) {
-        fprintf(stderr, "Błąd: Wystąpił problem podczas odczytu pliku (char mode).\n");
-        return EXIT_FAILURE;
-    }
-    
-    if (!feof(in_file)) {
-        fprintf(stderr, "Błąd: Pętla zakończyła się przed dotarciem do końca pliku.\n");
-        return EXIT_FAILURE;
-    }
-
+    if (ferror(in_file)) return EXIT_FAILURE;
+    if (!feof(in_file)) return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
 
 int process_by_line(FILE *in_file, FILE *out_screen, FILE *out_file) {
     char buf[BUFFER_SIZE_LINE];
-
     while (fgets(buf, sizeof(buf), in_file) != NULL) {
         fputs(buf, out_screen);
         fputs(buf, out_file); 
     }
-
-    if (ferror(in_file)) {
-        fprintf(stderr, "Błąd: Wystąpił problem podczas odczytu pliku (line mode).\n");
-        return EXIT_FAILURE;
-    }
-
-    if (!feof(in_file)) {
-        fprintf(stderr, "Błąd: Pętla zakończyła się przed dotarciem do końca pliku.\n");
-        return EXIT_FAILURE;
-    }
-
+    if (ferror(in_file)) return EXIT_FAILURE;
+    if (!feof(in_file)) return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
+
 int process_by_block(FILE *in_file, FILE *out_screen, FILE *out_file) {
     char buf[BUFFER_SIZE_BLOCK];
     size_t bytes_read;
-
     while ((bytes_read = fread(buf, sizeof(char), BUFFER_SIZE_BLOCK, in_file)) > 0) {
         fwrite(buf, sizeof(char), bytes_read, out_screen);
         fwrite(buf, sizeof(char), bytes_read, out_file);
     }
-
-    if (ferror(in_file)) {
-        fprintf(stderr, "Błąd: Wystąpił problem podczas odczytu pliku (block mode).\n");
-        return EXIT_FAILURE;
-    }
-
-    if (!feof(in_file)) {
-        fprintf(stderr, "Błąd: Pętla zakończyła się przed dotarciem do końca pliku.\n");
-        return EXIT_FAILURE;
-    }
-
+    if (ferror(in_file)) return EXIT_FAILURE;
+    if (!feof(in_file)) return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
 
 void print_output_file_content(const char *filename, FILE *out_file) {
     int c;
     rewind(out_file);
-
-    printf("%s:\n", filename);
-
-
+    printf("%s: ", filename);
     while ((c = fgetc(out_file)) != EOF) {
         fputc(c, stdout);
     }
